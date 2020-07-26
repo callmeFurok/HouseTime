61ba68054c91b183887265d7401f48e7d0e2f4f1
Furok-Dev <thefurok@gmail.com>
2020-07-26 15:57:46 -0500

    Agregamos el buscar y cotizar a anuncios

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(function() {
+    let buscar = document.getElementById('buscar');
+
+    buscar.addEventListener('click', function() {
+        if (buscar.value == "nombre") {
+            document.getElementById("nombre").style.display = "inherit";
+            document.getElementById("precio").style.display = "none";
+            document.getElementById("fecha").style.display = "none";
+            document.getElementById("ubicacion").style.display = "none";
+            document.getElementById("servicios").style.display = "none";
+            document.getElementById("capacidad").style.display = "none";
+        }
+        if (buscar.value == "precio") {
+            document.getElementById("precio").style.display = "inherit";
+            document.getElementById("nombre").style.display = "none";
+            document.getElementById("fecha").style.display = "none";
+            document.getElementById("ubicacion").style.display = "none";
+            document.getElementById("servicios").style.display = "none";
+            document.getElementById("capacidad").style.display = "none";
+        }
+        if (buscar.value == "fechas") {
+            document.getElementById("fecha").style.display = "inherit";
+            document.getElementById("precio").style.display = "none";
+            document.getElementById("nombre").style.display = "none";
+            document.getElementById("ubicacion").style.display = "none";
+            document.getElementById("servicios").style.display = "none";
+            document.getElementById("capacidad").style.display = "none";
+        }
+        if (buscar.value == "ubicacion") {
+            document.getElementById("ubicacion").style.display = "inherit";
+            document.getElementById("fecha").style.display = "none";
+            document.getElementById("precio").style.display = "none";
+            document.getElementById("nombre").style.display = "none";
+            document.getElementById("servicios").style.display = "none";
+            document.getElementById("capacidad").style.display = "none";
+        }
+        if (buscar.value == "servicios") {
+            document.getElementById("servicios").style.display = "inherit";
+            document.getElementById("ubicacion").style.display = "none";
+            document.getElementById("fecha").style.display = "none";
+            document.getElementById("precio").style.display = "none";
+            document.getElementById("nombre").style.display = "none";
+            document.getElementById("capacidad").style.display = "none";
+        }
+        if (buscar.value == "capacidad") {
+            document.getElementById("capacidad").style.display = "inherit";
+            document.getElementById("servicios").style.display = "none";
+            document.getElementById("ubicacion").style.display = "none";
+            document.getElementById("fecha").style.display = "none";
+            document.getElementById("precio").style.display = "none";
+            document.getElementById("nombre").style.display = "none";
+        }
+    });
+});
