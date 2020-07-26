57861834b548e750e0a5fe25ae6066fb63ca4743
stalinchiguano98 <59268073+stalinchiguano98@users.noreply.github.com>
2020-07-26 17:00:25 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -51,4 +51,15 @@ $(function() {
             document.getElementById("nombre").style.display = "none";
         }
     });
+    let latitud = document.getElementById('latitud');
+    let longitud = document.getElementById('longitud');
+
+    var map = L.map('map').setView([parseFloat(latitud.value), parseFloat(longitud.value)], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([parseFloat(latitud.value), parseFloat(longitud.value)]).addTo(map)
+        .openPopup();
 });
