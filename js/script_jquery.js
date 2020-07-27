dcc2c610a95553da4fde7c70aad6a6567d6ba21a
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-07-26 21:32:06 -0500

    Agregado los contadores en el index

---
+++ js/script_jquery.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('#clientes').animateNumber({ number: 120 }, 12000);
+    $('#viajes').animateNumber({ number: 2500 }, 6000);
+    $('#paquetes').animateNumber({ number: 50 }, 25000);
+    $('#promociones').animateNumber({ number: 20 }, 30000);
+});
